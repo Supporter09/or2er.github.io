b2d856719e05e4076be891fca768a434c56cc810
nhatminh-selfomy <maivannhatminh@selfomy.com>
2025-05-06 22:11:00 +0700

    fix: setting disOut

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     reactStrictMode: true,
-    distDir: "out", // change output directory to 'out' instead of '.next'.
 };
 
 export default nextConfig;
